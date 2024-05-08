109d8ebba799b3028aecac19f290d522e83fe6b8
Mayank Chaudhary <91132078+mynkchaudhry@users.noreply.github.com>
2024-05-08 17:25:41 +0530

    all set

--- src/services/article.js
+++ src/services/article.js
@@ -14,7 +14,7 @@ export const articleApi = createApi({
     }),
     endpoints: (builder) => ({
         getSummary: builder.query({
-            query: (params) => `/summarize?url=${encodeURIComponent(params.articleUrl)}&length=10`,
+            query: (params) => `/summarize?url=${encodeURIComponent(params.articleUrl)}&length=20`,
             onError: (error) => {
                 console.error('An error occurred:', error);
                 // Handle error (e.g., show error message to user)
